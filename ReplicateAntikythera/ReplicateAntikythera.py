1cdd297c3234e0568bc709cde17fc1dc47350478
Alexander Puttre <apputtre@gmail.com>
2023-05-25 09:47:19 -0400

    Added definitions for classes

--- ReplicateAntikythera/ReplicateAntikythera.py
+++ ReplicateAntikythera/ReplicateAntikythera.py
@@ -1,2 +1,19 @@
-
 print("Live, love, laugh, Python :)")
+
+class Satellite:
+    def __init__(self):
+        print("Satellite constructor")
+
+class Planet(Satellite):
+    def __init__(self):
+        print("Planet constructor")
+
+class Moon(Satellite):
+    def __init__(self):
+        print("Moon constructor")
+
+class Asteroid:
+    def __init__(self):
+        print("Asteroid constructor")
+
+p = Planet();
