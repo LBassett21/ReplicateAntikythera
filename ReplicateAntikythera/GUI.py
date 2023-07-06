cfe4864233fd1097d8de48b3c26a7ed70dc1b696
Luke Bassett <bassettl@wit.edu>
2023-07-06 11:25:21 -0400

    Added scroll zoom in, click and drag, updated key, scaling functionality

--- ReplicateAntikythera/GUI.py
+++ ReplicateAntikythera/GUI.py
@@ -13,7 +13,7 @@ pygame.init()
 
 # Set up the display
 width, height = pygame.display.Info().current_w, pygame.display.Info().current_h #size of the display.
-screen = pygame.display.set_mode((width,height),pygame.FULLSCREEN) #creating pygame screen with width and height
+screen = pygame.display.set_mode((0,0),pygame.RESIZABLE) #creating pygame screen with width and height
 pygame.display.set_caption("2D Solar System") #title
 
 start_window = pygame.Surface((width, height))
@@ -99,12 +99,12 @@ key_text = {
     }
 
 pause_button_rect = pygame.Rect(width - 100, 10, 90, 30)
-pause_button_text = key_font.render("Pause", True, BLACK)
+pause_button_text = key_font.render("Pause", True, WHITE)
 
 paused = False;
 
 quit_button_rect = pygame.Rect(width - 200, 10, 90, 30)
-quit_button_text = key_font.render("Exit", True, BLACK)
+quit_button_text = key_font.render("Exit", True, WHITE)
 
 # Define asteroid belt properties
 asteroid_radius = 2
@@ -126,8 +126,39 @@ running = True
 clock = pygame.time.Clock()
 
 show_start_window = True
+dragging = False
+
+# Define initial constants
+zoom_scale = 1.0
+offset_x = 0
+offset_y = 0
+
+# Define the fixed point for zooming
+zoom_center_x = width // 2
+zoom_center_y = height // 2
 
 while running:
+    # Scale the screen
+    scaled_width = int(width * zoom_scale)
+    scaled_height = int(height * zoom_scale)
+    scaled_screen = pygame.Surface((scaled_width, scaled_height))
+
+    # Scale the background image
+    scaled_background_image = pygame.transform.smoothscale(background_image, (scaled_width, scaled_height))
+
+    # Scale the start window and text
+    scaled_start_text = pygame.transform.smoothscale(start_text, (scaled_width, scaled_height))
+  
+    # Calculate the center of the scaled screen
+    scaled_center_x = scaled_width // 2
+    scaled_center_y = scaled_height // 2
+
+    # Calculate the mouse cursor position relative to the scaled screen
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    scaled_mouse_x = (mouse_x - scaled_center_x) / zoom_scale + scaled_center_x
+    scaled_mouse_y = (mouse_y - scaled_center_y) / zoom_scale + scaled_center_y
+
+
     # Process events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -139,6 +170,19 @@ while running:
                 paused = not paused
             elif show_start_window and start_text_rect.collidepoint(event.pos):
                 show_start_window = False
+            elif not dragging:
+                dragging = True
+        elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+            if dragging:
+                dragging = False
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 4 and not show_start_window:
+            zoom_scale *= 1.05
+            offset_x = zoom_center_x - scaled_mouse_x / zoom_scale
+            offset_y = zoom_center_y - scaled_mouse_y / zoom_scale
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 5 and not show_start_window:
+            zoom_scale /= 1.05
+            offset_x = zoom_center_x - scaled_mouse_x / zoom_scale
+            offset_y = zoom_center_y - scaled_mouse_y / zoom_scale
 
     if show_start_window:
         screen.blit(start_window, (0, 0))
@@ -151,35 +195,40 @@ while running:
 
         pygame.display.flip()
         continue
-    screen.blit(background_image,(0,0))
 
-    # Clear the screen
-    #screen.fill(BLACK)
+    # Move/Offset the screen if dragging is true
+    if dragging:
+        offset_x += mouse_x - prev_mouse_x
+        offset_y += mouse_y - prev_mouse_y
+    prev_mouse_x = mouse_x
+    prev_mouse_y = mouse_y
 
     if not paused:
     # Update planet positions
-        earth_x = sun_pos[0] + math.cos(earth_angle) * earth_distance
-        earth_y = sun_pos[1] + math.sin(earth_angle) * earth_distance
+        scaled_sun_pos = (scaled_center_x + offset_x, scaled_center_y + offset_y)
+
+        earth_x = scaled_sun_pos[0] + math.cos(earth_angle) * earth_distance
+        earth_y = scaled_sun_pos[1] + math.sin(earth_angle) * earth_distance
         earth_angle += earth_speed
 
         moon_x = earth_x + math.cos(moon_angle) * moon_distance
         moon_y = earth_y + math.sin(moon_angle) * moon_distance
         moon_angle += moon_speed
 
-        mars_x = sun_pos[0] + math.cos(mars_angle) * mars_distance
-        mars_y = sun_pos[1] + math.sin(mars_angle) * mars_distance
+        mars_x = scaled_sun_pos[0] + math.cos(mars_angle) * mars_distance
+        mars_y = scaled_sun_pos[1] + math.sin(mars_angle) * mars_distance
         mars_angle += mars_speed
 
-        venus_x = sun_pos[0] + math.cos(venus_angle) * venus_distance
-        venus_y = sun_pos[1] + math.sin(venus_angle) * venus_distance
+        venus_x = scaled_sun_pos[0] + math.cos(venus_angle) * venus_distance
+        venus_y = scaled_sun_pos[1] + math.sin(venus_angle) * venus_distance
         venus_angle += venus_speed
 
-        mercury_x = sun_pos[0] + math.cos(mercury_angle) * mercury_distance
-        mercury_y = sun_pos[1] + math.sin(mercury_angle) * mercury_distance
+        mercury_x = scaled_sun_pos[0] + math.cos(mercury_angle) * mercury_distance
+        mercury_y = scaled_sun_pos[1] + math.sin(mercury_angle) * mercury_distance
         mercury_angle += mercury_speed
 
-        jupiter_x = sun_pos[0] + math.cos(jupiter_angle) * jupiter_distance
-        jupiter_y = sun_pos[1] + math.sin(jupiter_angle)
+        jupiter_x = scaled_sun_pos[0] + math.cos(jupiter_angle) * jupiter_distance
+        jupiter_y = scaled_sun_pos[1] + math.sin(jupiter_angle)
         jupiter_angle += jupiter_speed
 
     #Draw the rotating lines for the planet
@@ -187,84 +236,73 @@ while running:
     #pygame.draw.line(screen, WHITE, sun_pos, (mars_x, mars_y), 1)
 
     # Clear the line positions
-        zodiac_line_points.clear()
+    zodiac_line_points.clear()
 
     # Calculate the zodiac line endpoints
     for i in range(12):  # Assuming there are 12 zodiac signs
-        angle = earth_angle + (i * math.pi / 6)  # Angle for each zodiac sign
+        angle = (i * math.pi / 6)  # Angle for each zodiac sign
         line_x = earth_x + math.cos(angle) * 50  # Adjust the length of the lines as needed
         line_y = earth_y + math.sin(angle) * 50
         zodiac_line_points.append((line_x, line_y))
 
+    scaled_screen.blit(scaled_background_image, (offset_x, offset_y))
+
     # Draw the sun
-    pygame.draw.circle(screen, YELLOW, sun_pos, sun_radius)
+    pygame.draw.circle(scaled_screen, YELLOW, (int(scaled_sun_pos[0]),int(scaled_sun_pos[1])), int(sun_radius//zoom_scale))
 
     #Draw the moon
-    pygame.draw.circle(screen, WHITE, (int(moon_x), int (moon_y)),moon_radius)
+    pygame.draw.circle(scaled_screen, WHITE, (int(moon_x), int (moon_y)),int(moon_radius//zoom_scale))
 
     # Draw the planets
-    pygame.draw.circle(screen, BLUE, (int(earth_x), int(earth_y)), earth_radius)
-    pygame.draw.circle(screen, RED, (int(mars_x), int(mars_y)), mars_radius)
-    pygame.draw.circle(screen, GREEN, (int(venus_x), int(venus_y)), venus_radius)
-    pygame.draw.circle(screen, ORANGE, (int(mercury_x), int(mercury_y)),mercury_radius)
-    pygame.draw.circle(screen, PURPLE, (int(jupiter_x), int(jupiter_y)),jupiter_radius)
+    pygame.draw.circle(scaled_screen, BLUE, (int(earth_x), int(earth_y)), int(earth_radius//zoom_scale))
+    pygame.draw.circle(scaled_screen, RED, (int(mars_x), int(mars_y)), int(mars_radius//zoom_scale))
+    pygame.draw.circle(scaled_screen, GREEN, (int(venus_x), int(venus_y)), int(venus_radius//zoom_scale))
+    pygame.draw.circle(scaled_screen, ORANGE, (int(mercury_x), int(mercury_y)),int(mercury_radius//zoom_scale))
+    pygame.draw.circle(scaled_screen, PURPLE, (int(jupiter_x), int(jupiter_y)),int(jupiter_radius//zoom_scale))
 
     # Draws the lines around the earth for zodiac signs
     for line_point in zodiac_line_points:
-        pygame.draw.line(screen, (128, 128, 128), (int(earth_x), int(earth_y)), line_point, 1)
-
-
-    #Draw the key
-    key_x =10
-    key_y = 10
-    key_padding = 20
-
-    for i, (planet, color) in enumerate(key_text.items()):
-        key_surface = key_font.render(f"{planet}: {color}", True, WHITE)
-        screen.blit(key_surface, (key_x, key_y + i * key_padding))
-
-    # Calculates the current sign
-        current_sign_index = int((earth_angle / (2 * math.pi)) * len(zodiac_signs)) % len(zodiac_signs)
-        current_sign = zodiac_signs[current_sign_index]
-
-        text_box = pygame.Surface((200, 30))
-        text_box.fill(BLACK)
-        text_surface = key_font.render(f"Current Sign: {current_sign}", True, WHITE)
-        text_box.blit(text_surface, (10, 5))
-
-    screen.blit(text_box, (key_x, key_y + len(key_text) * key_padding))
+        pygame.draw.line(scaled_screen, (128, 128, 128), (int(earth_x), int(earth_y)), line_point, 1)
 
     for asteroid in asteroids:
         asteroid_distance, asteroid_speed, asteroid_angle = asteroid
-        asteroid_x = sun_pos[0] + math.cos(asteroid_angle) * asteroid_distance
-        asteroid_y = sun_pos[1] + math.sin(asteroid_angle) * asteroid_distance
+        asteroid_x = scaled_sun_pos[0] + math.cos(asteroid_angle) * asteroid_distance
+        asteroid_y = scaled_sun_pos[1] + math.sin(asteroid_angle) * asteroid_distance
         asteroid_angle += asteroid_speed
         asteroid = (asteroid_distance, asteroid_speed, asteroid_angle)
 
-        pygame.draw.circle(screen, WHITE, (int(asteroid_x), int(asteroid_y)), asteroid_radius)
+        pygame.draw.circle(scaled_screen, WHITE, (int(asteroid_x), int(asteroid_y)), asteroid_radius)
 
     # Update the asteroids list
     asteroids = [asteroid for asteroid in asteroids if asteroid[0] >= asteroid_distance_min]
 
-    pygame.draw.rect(screen, RED, pause_button_rect)
-    screen.blit(pause_button_text, pause_button_rect.move(30,10))
+    # Update the display with the scaled screen
+    screen.blit(pygame.transform.scale(scaled_screen, (width, height)), (0, 0))
 
-    pygame.draw.rect(screen, RED, quit_button_rect)
-    screen.blit(quit_button_text, quit_button_rect.move(30,10))
+    # Add a pause button on top of the scaled screen
+    pygame.draw.rect(screen, RED, pause_button_rect, 1)
+    screen.blit(pause_button_text, (pause_button_rect.x, pause_button_rect.y))
 
-    # Display the current date and time
-    days_per_rotation = 1  # Number of days it takes for Earth to make a full rotation around the Sun
-    moon_speed = -0.1  # Moon's speed of rotation
-    moon_days_per_rotation = days_per_rotation / abs(moon_speed)  # Number of days per Moon rotation
-    moon_angle_normalized = moon_angle % (2 * math.pi)  # Normalize the moon angle
-    moon_time_delta = timedelta(days=(moon_angle_normalized / (2 * math.pi)) * moon_days_per_rotation)
-    current_date = ref_date + moon_time_delta
+    # Add a exit button on top of the scaled screen
+    pygame.draw.rect(screen, RED, quit_button_rect, 1)
+    screen.blit(quit_button_text, (quit_button_rect.x, quit_button_rect.y))
 
-    date_text = key_font.render(f"Date: {current_date.date()}", True, WHITE)
-    time_text = key_font.render(f"Time: {current_date.strftime('%H:%M:%S')}", True, WHITE)
-    screen.blit(date_text, (10, height - 40))
-    screen.blit(time_text, (10, height - 20))
-    # Update the display
+    #Draw the key
+    key_x = 10
+    key_y = 10
+    key_padding = 20
+
+    # Calculates the current sign
+    current_sign_index = int((earth_angle / (2 * math.pi)) * len(zodiac_signs)) % len(zodiac_signs)
+    current_sign = zodiac_signs[current_sign_index]
+
+    for i, (planet, color) in enumerate(key_text.items()):
+        if planet == "Earth":
+            planet_text = f"{planet}: {color} ({current_sign})"
+        else:
+            planet_text = f"{planet}: {color}"
+        key_surface = key_font.render(planet_text, True, WHITE)
+        screen.blit(key_surface, (key_x, key_y + i * key_padding))
 
     pygame.display.flip()
     clock.tick(40)
